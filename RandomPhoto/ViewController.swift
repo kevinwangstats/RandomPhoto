ec800390e880128d6bae58e1832beee4542c838f
Kevin Wang <92612961+kevinwangstats@users.noreply.github.com>
2024-01-08 13:07:14 +1100

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Kevin Wang on 8/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
